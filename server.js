a3fc55f63ce9c345244e25fedaac5a49915f2966
Sai-guru <lpnkaveri@gmail.com>
2025-07-20 17:54:39 +0530

    done the env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/',(req,res)=> {
